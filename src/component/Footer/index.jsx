81fc5d2a8bb45f70cc9b499e37b4778e78850e10
SnB <segey.nebo@gmail.com>
2019-08-27 15:14:16 +0300

    fixes shrink for contact block

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -4,14 +4,14 @@ import style from './style.css'
 
 const Footer = () => (
     <footer className={style.footer}>
-        <Section title="Contact us">
+        <Section title="Contact us" className={style.section}>
             <a name="contact"></a>
             <form className={style.footer__form}>
                 <input type="email" placeholder="Enter email" />
                 <button type="submit">Submit</button>
             </form>
         </Section>
-        <Section title="Address">
+        <Section title="Address" className={style.section}>
             <address> Some street in Lousiana </address>
         </Section>
     </footer>
