c3a5a74fdca19e03059fe457737cd5511fcef730
snb <sergey.belko@tut.by>
2019-09-20 15:00:21 +0300

    continue implemet flexsections

--- src/component/FlexSection/index.jsx
+++ src/component/FlexSection/index.jsx
@@ -1,17 +1,21 @@
 import { Section } from '@component'
-import { addKeyToItem, updateArr } from '@util'
+import { updateArr } from '@util'
 import React from 'react'
 import style from './style.css'
 
 const Option = ({ title, ...rest }) => <option {...rest}>{title}</option>
 
-const Element = ({ key, ...rest }) => <div className={style.element} key={key}>{console.log({ rest })}</div>
+const Element = ({ key, ...rest }) => <div className={style.element} key={key}>{rest}</div>
 
 const FlexDetails = ({ summary, options = [] }) => (
     <details>
         {summary && <summary>{summary}</summary>}
-        <select defaultChecked={options.find((item) => item.checked)}>{options.map(addKeyToItem).map(Option)}</select>
-        <div className={style.container}>{updateArr({ arr: [...new Array(4).keys()], component: Element })}</div>
+        <select defaultChecked={options.find((item) => item.checked)}>
+            {updateArr({ arr: options, component: Option })}
+        </select>
+        <div className={style.container}>
+            {updateArr({ arr: [...new Array(4).keys()], component: Element })}
+        </div>
     </details>
 )
 
