e4ed1a76bf555d36fd86bb2065566cef07b13845
SnB <segey.nebo@gmail.com>
2019-09-23 09:40:31 +0300

    add react context

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,5 +1,5 @@
 import { inject as mobxInject, observer, Provider } from "mobx-react"
-import React from "react"
+import { createContext, default as React, useContext } from "react"
 import Main from './main'
 import User from './user'
 
@@ -7,6 +7,24 @@ const store = {
   User,
   Main
 }
+
+const initialState = { Main, User }
+
+const StoreContext = createContext(initialState)
+
+export const StoreProvider1 = ({ children }) => {
+  return (
+    <StoreContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  )
+}
+
+export const useStore = store => {
+  const { state, dispatch } = useContext(StoreContext)
+  return state[store]
+}
+
 
 export const StoreProvider = ({ children }) => (
   <Provider {...store}>
